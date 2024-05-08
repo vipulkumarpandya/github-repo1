495d7534efdb076d5b9784cd1f02f33d626f8452
Vipul Pandya <vipulkumarpandya@gmail.com>
2024-05-08 11:23:27 -0500

    Initial code commit

---
+++ src/test/java/TestRunner/TestRunnerClass.java
@@ -0,0 +1,28 @@
+package TestRunner;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions (features = "src/main/resources/featurefiles", 
+					glue = {"stepDefFiles"},
+					plugin = {"pretty", "html:/target/cucumber-reports", "json:target/cucumber.json"},
+					monochrome = true,
+					publish = true
+		)
+
+
+public class TestRunnerClass extends AbstractTestNGCucumberTests{
+	
+	@Override 
+	@DataProvider(parallel = true)
+	public Object[][] scenarios() {
+		
+		Object[][] cnt = super.scenarios();
+		System.out.println("No. of scenarios = " + cnt.length);
+		
+		return cnt;
+	}
+
+}
---
+++ src/test/java/stepDefFiles/apptestStepDef.java
@@ -0,0 +1,16 @@
+package stepDefFiles;
+
+import io.cucumber.java.en.Given;
+
+public class apptestStepDef {
+	
+	
+	
+	@Given("I want to run a sample BDD test")
+	public void i_want_to_run_a_sample_bdd_test() {
+	    System.out.println("From - I want to run a sample BDD test method");
+	}
+	
+	
+
+}
